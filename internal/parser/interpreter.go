5516aa4dd0734925eaa3f5f45a470e7379897141
Mikhailo <144545017+MikhailoSafronov@users.noreply.github.com>
2025-05-29 10:36:18 +0300

    code review

--- internal/parser/interpreter.go
+++ internal/parser/interpreter.go
@@ -7,9 +7,9 @@ import (
 	"github.com/MikhailoSafronov/Project-lab4/internal/lexer"
 )
 
-// Interpret виконує обробку AST
+// Interpret виконує обробку AST (абстрактного синтаксичного дерева).
 func Interpret(nodes []ast.Node) {
-	const tapeSize = 30000
+	const tapeSize = 30000 // Стандартна довжина стрічки
 	tape := make([]byte, tapeSize)
 	ptr := 0
 
@@ -19,31 +19,31 @@ func Interpret(nodes []ast.Node) {
 			switch node := n.(type) {
 			case *ast.Command:
 				switch node.Kind {
-				case lexer.GT:
+				case lexer.GT: // >
 					ptr++
 					if ptr >= tapeSize {
-						ptr = 0 // wrap around
+						ptr = 0 // обгортання в кінець
 					}
-				case lexer.LT:
+				case lexer.LT: // <
 					if ptr == 0 {
-						ptr = tapeSize - 1
+						ptr = tapeSize - 1 // обгортання в початок
 					} else {
 						ptr--
 					}
-				case lexer.PLUS:
+				case lexer.PLUS: // +
 					tape[ptr]++
-				case lexer.MINUS:
+				case lexer.MINUS: // -
 					tape[ptr]--
-				case lexer.DOT:
+				case lexer.DOT: // .
 					fmt.Printf("%c", tape[ptr])
-				case lexer.COMMA:
+				case lexer.COMMA: // ,
 					var input byte
 					fmt.Scanf("%c", &input)
 					tape[ptr] = input
 				}
 			case *ast.Loop:
 				for tape[ptr] != 0 {
-					exec(node.Body)
+					exec(node.Body) // рекурсивне виконання тіла циклу
 				}
 			}
 		}
